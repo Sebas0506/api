c7f31de7d2c1365eee7264c0424abe4d407a3249
Sebastián <sgonzalez34@ucol.mx>
2022-03-19 12:13:36 -0600

    Segundo Commit

---
+++ app/models/client.js
@@ -0,0 +1,9 @@
+class client{
+    constructor(name,rfc,zipcode){
+        this.id=null;
+        this.name=name;
+        this.rfc=rfc;
+        this.zipcode=zipcode;
+    }
+}
+module.exports=client;
