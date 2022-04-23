74f39015092cec650db7d428be93165c4b5de646
JaimeJuarez <jjuarez17@ucol.mx>
2022-04-23 17:25:40 -0500

    Quinto Commit

--- app/routes/client.js
+++ app/routes/client.js
@@ -1,15 +1,15 @@
 var clientController=require('../controllers/clientController');
 var router = require('express').Router()
 router.get('/', function(req, res) {
-  
+
     clientController.list(req,res);
 })
 router.get('/:id', function(req, res) {
-  
+
     clientController.find(req,res);
 })
 router.post('/', function(req, res) {
-  
+
     clientController.create(req,res);
 })
 //url:puerto/api/clientes
